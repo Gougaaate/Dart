274c9643f75a9b5a56a3b50daa808db1a592a1e1
adam <adam.goux--gateau@ensta-bretagne.org>
2022-11-28 14:37:36 +0100

    je prie

--- py/robot_cmd_1.py
+++ py/robot_cmd_1.py
@@ -3,12 +3,25 @@ import time
 if __name__ == "__main__":
 
     mybot = dartv2b.DartV2()
-    right, left = mybot.get_front_encoders()
-    a = time.time()
-    while mybot.get_front_encoders()[0] - right < 300:
-        mybot.set_speed(100,-100)
-        t = time.time() - a
-        print(f'Le temps requis est {t} secondes')
-    mybot.set_speed(0, 0)
 
+    test = True
+    mybot.sonars.
+    while test:
+        print(mybot.get_cardinal_sonars()[0], mybot.get_cardinal_sonars()[1], mybot.get_cardinal_sonars()[2], mybot.get_cardinal_sonars()[3])
+        mybot.set_speed(100,100)
+        if mybot.get_cardinal_sonars()[0] < 10:
+            test =False
+    # right, left = mybot.get_front_encoders()
+    # if right < 65236:
+    #     while mybot.get_front_encoders()[0] - right < 240:
+    #         mybot.set_speed(100,-100)
+    #
+    # else:
+    #     while (mybot.get_front_encoders()[0] > - 65535 and mybot.get_front_encoders()[0] < -131071 + 240 + right) or (mybot.get_front_encoders()[0]  > right) :
+    #         mybot.set_speed(100, -100)
+    #         print (mybot.get_front_encoders()[0])
+
+    mybot.set_speed(0,0)
     mybot.end()
+
+
