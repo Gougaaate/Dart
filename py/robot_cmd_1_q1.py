8fdc960bee2e7fae0253ac0f4d03cafaa3aa148a
adam <adam.goux--gateau@ensta-bretagne.org>
2022-11-28 17:40:31 +0100

    commit du README

--- py/robot_cmd_1_q1.py
+++ py/robot_cmd_1_q1.py
@@ -4,21 +4,22 @@ import time
 
 if __name__ == "__main__":
     mybot = drv2.DartV2DriverV2()
-    mybot.powerboard.set_speed(80,80)
+    mybot.powerboard.set_speed(40,40)
+    print('coucou je roule')
     time.sleep(5)
-    # mybot.sonars.init_4_sonars()
-    # test = True
-    # while test:
-    #     mybot.powerboard.set_speed(80,80)
-    #     print(f"front = {mybot.sonars.read_4_sonars()[0]} left = {mybot.sonars.read_4_sonars()[1]} right = {mybot.sonars.read_4_sonars()[3]}")
-    #     if mybot.sonars.read_4_sonars()[0] < 60:
-    #         time.sleep(1)
-    #         if mybot.sonars.read_4_sonars()[1] <= mybot.sonars.read_4_sonars()[3]:
-    #             print("turn right")
-    #             mybot.turn_right()
-    #         else:
-    #             print("turn left")
-    #             mybot.turn_left()
+    mybot.sonars.init_4_sonars()
+    test = True
+    while test:
+        mybot.powerboard.set_speed(80,80)
+        # print(f"front = {mybot.sonars.read_4_sonars()[0]} left = {mybot.sonars.read_4_sonars()[1]} right = {mybot.sonars.read_4_sonars()[3]}")
+        if mybot.sonars.read_4_sonars()[0] < 70:
+            time.sleep(1)
+            if mybot.sonars.read_4_sonars()[1] <= mybot.sonars.read_4_sonars()[3]:
+                print("turn right")
+                mybot.turn_right()
+            else:
+                print("turn left")
+                mybot.turn_left()
 
 
     mybot.powerboard.set_speed(0,0)
