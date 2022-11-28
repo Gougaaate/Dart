065a107375a3845d67f56d80234e7e4f1f219704
adam <adam.goux--gateau@ensta-bretagne.org>
2022-11-28 17:23:04 +0100

    oubli d'un fichier

--- py/robot_cmd_1_q1.py
+++ py/robot_cmd_1_q1.py
@@ -4,19 +4,21 @@ import time
 
 if __name__ == "__main__":
     mybot = drv2.DartV2DriverV2()
-    mybot.sonars.init_4_sonars()
-    test = True
-    while test:
-        mybot.powerboard.set_speed(80,80)
-        print(f"front = {mybot.sonars.read_4_sonars()[0]} left = {mybot.sonars.read_4_sonars()[1]} right = {mybot.sonars.read_4_sonars()[3]}")
-        if mybot.sonars.read_4_sonars()[0] < 60:
-            time.sleep(1)
-            if mybot.sonars.read_4_sonars()[1] <= mybot.sonars.read_4_sonars()[3]:
-                print("turn right")
-                mybot.turn_right()
-            else:
-                print("turn left")
-                mybot.turn_left()
+    mybot.powerboard.set_speed(80,80)
+    time.sleep(5)
+    # mybot.sonars.init_4_sonars()
+    # test = True
+    # while test:
+    #     mybot.powerboard.set_speed(80,80)
+    #     print(f"front = {mybot.sonars.read_4_sonars()[0]} left = {mybot.sonars.read_4_sonars()[1]} right = {mybot.sonars.read_4_sonars()[3]}")
+    #     if mybot.sonars.read_4_sonars()[0] < 60:
+    #         time.sleep(1)
+    #         if mybot.sonars.read_4_sonars()[1] <= mybot.sonars.read_4_sonars()[3]:
+    #             print("turn right")
+    #             mybot.turn_right()
+    #         else:
+    #             print("turn left")
+    #             mybot.turn_left()
 
 
     mybot.powerboard.set_speed(0,0)
