fed48d51b960207f56817a85f4fd891faf7ed4ea
gillarlo <louis.gillard@ensta-bretagne.org>
2022-12-09 09:25:32 +0100

    Ajout d'un temps fixe de boucle (avec sleep)

--- py/robot_cmd_1.py
+++ py/robot_cmd_1.py
@@ -5,14 +5,14 @@ from log import Log
 
 
 if __name__ == "__main__":
-    Log.dt = .1
+    dt = .2
+    Log.dt = dt
     log = Log()
 
     mybot = drv2.DartV2DriverV2()
     last_delta = mybot.sonars.read_right() - mybot.sonars.read_left()
 
     def go_straight(last_delta):
-        dt = .1
         kp, ki, kd = .23, 1.7, .02
         left, right = mybot.sonars.read_left(), mybot.sonars.read_right()
         print(f"")
@@ -25,17 +25,25 @@ if __name__ == "__main__":
         print(f"{left = }, {right = }, P = {kp*delta[1]:.2f}, I = {ki*(delta[0] + delta[1])*dt:.2f}, D = {kd*(delta[0] - delta[1])/dt:.2f}")
         s = kp*delta[1] + ki*(delta[0] + delta[1])*dt + kd*(delta[0] - delta[1])/dt
         mybot.powerboard.set_speed(100 + s  , 100 - s)
+
         return delta[1]
         
 
     test = True
 
     while test:
+        t0_loop = time.time()
         Log.current_data = []
         last_delta = go_straight(last_delta)
         Log.write_current_data()
         # if mybot.sonars.read_4_sonars()[0] < 10:
         #     test = False
+
+        t1_loop = time.time()
+        dt_loop = t1_loop - t0_loop
+        dt_sleep = dt - dt_loop
+        if dt_sleep > 0:
+            time.sleep(dt_sleep)
 
     right, left = mybot.encoders.read_encoders()
 
