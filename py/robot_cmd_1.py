f22b7b48d6ac23ea0b9233a5120585095e28b4f1
gillarlo <louis.gillard@ensta-bretagne.org>
2022-12-09 08:34:23 +0100

    Début de go_straight

--- py/robot_cmd_1.py
+++ py/robot_cmd_1.py
@@ -1,27 +1,52 @@
-import dartv2b
+import drivers_v2.drivers_v2 as drv2
 import time
-if __name__ == "__main__":
+from log import Log
 
-    mybot = dartv2b.DartV2()
+
+
+if __name__ == "__main__":
+    Log.dt = .1
+    log = Log()
+
+    mybot = drv2.DartV2DriverV2()
+    last_delta = mybot.sonars.read_right() - mybot.sonars.read_left()
+
+    def go_straight(last_delta):
+        dt = .1
+        kp, ki, kd = .23, 1.7, .02
+        left, right = mybot.sonars.read_left(), mybot.sonars.read_right()
+        print(f"")
+        delta = [last_delta, right - left]
+        Log.add_to_current_data(left=left)
+        Log.add_to_current_data(right=right)
+        Log.add_to_current_data(P=kp*delta[1])
+        Log.add_to_current_data(I=ki*(delta[0] + delta[1])*dt)
+        Log.add_to_current_data(D=kd*(delta[0] - delta[1])/dt)
+        print(f"{left = }, {right = }, P = {kp*delta[1]:.2f}, I = {ki*(delta[0] + delta[1])*dt:.2f}, D = {kd*(delta[0] - delta[1])/dt:.2f}")
+        s = kp*delta[1] + ki*(delta[0] + delta[1])*dt + kd*(delta[0] - delta[1])/dt
+        mybot.powerboard.set_speed(100 + s  , 100 - s)
+        return delta[1]
+        
 
     test = True
-    mybot.sonars.
+
     while test:
-        print(mybot.get_cardinal_sonars()[0], mybot.get_cardinal_sonars()[1], mybot.get_cardinal_sonars()[2], mybot.get_cardinal_sonars()[3])
-        mybot.set_speed(100,100)
-        if mybot.get_cardinal_sonars()[0] < 10:
-            test =False
-    # right, left = mybot.get_front_encoders()
-    # if right < 65236:
-    #     while mybot.get_front_encoders()[0] - right < 240:
-    #         mybot.set_speed(100,-100)
-    #
-    # else:
-    #     while (mybot.get_front_encoders()[0] > - 65535 and mybot.get_front_encoders()[0] < -131071 + 240 + right) or (mybot.get_front_encoders()[0]  > right) :
-    #         mybot.set_speed(100, -100)
-    #         print (mybot.get_front_encoders()[0])
+        Log.current_data = []
+        last_delta = go_straight(last_delta)
+        Log.write_current_data()
+        # if mybot.sonars.read_4_sonars()[0] < 10:
+        #     test = False
 
-    mybot.set_speed(0,0)
+    right, left = mybot.encoders.read_encoders()
+
+    if right < 65236:
+        while mybot.encoders.read_encoders()[0] - right < 240:
+            mybot.powerboard.set_speed(100, -100)
+
+    else:
+        while (mybot.encoders.read_encoders()[0] > - 65535 and mybot.encoders.read_encoders()[0] < -131071 + 240 + right) or (mybot.encoders.read_encoders()[0] > right):
+            mybot.powerboard.set_speed(100, -100)
+            print(mybot.encoders.read_encoders()[0])
+
+    mybot.powerboard.set_speed(0, 0)
     mybot.end()
-
-
