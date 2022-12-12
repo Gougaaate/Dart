f9ab09ec7fc92139aa96837685ab0e0d21a2a7a8
adam <adam.goux--gateau@ensta-bretagne.org>
2022-12-12 09:21:43 +0100

    "Milieu de séance, test du PID"

--- py/robot_cmd_1.py
+++ py/robot_cmd_1.py
@@ -3,6 +3,26 @@ import time
 from log import Log
 
 
+def go_straight(last_delta):
+    kp, ki, kd = 0, 0, 0
+    left, right = mybot.sonars.read_left(), mybot.sonars.read_right()
+
+    if left + right < 60:
+        kp, ki, kd = 1, 0, 0
+        # print(f"")
+    delta = [last_delta, right - left]
+    Log.add_to_current_data(left=left)
+    Log.add_to_current_data(right=right)
+    Log.add_to_current_data(P=kp * delta[1])
+    Log.add_to_current_data(I=ki * (delta[0] + delta[1]) * dt)
+    Log.add_to_current_data(D=kd * (delta[0] - delta[1]) / dt)
+
+    # print(f"{left = }, {right = }, P = {kp * delta[1]:.2f}, I = {ki * (delta[0] + delta[1]) * dt:.2f}, D = {kd * (delta[0] - delta[1]) / dt:.2f}")
+    s = kp * delta[1] + ki * (delta[0] + delta[1]) * dt - kd * (delta[0] - delta[1]) / dt
+    mybot.powerboard.set_speed(100 + s, 100 - s)
+
+    return delta[1]
+
 
 if __name__ == "__main__":
     dt = .1
@@ -12,29 +32,6 @@ if __name__ == "__main__":
     mybot = drv2.DartV2DriverV2()
     last_delta = mybot.sonars.read_right() - mybot.sonars.read_left()
 
-    def go_straight(last_delta):
-
-        kp, ki, kd = 0, 0, 0
-        left, right = mybot.sonars.read_left(), mybot.sonars.read_right()
-
-        if left + right < 60:
-            kp, ki, kd = .4, 2 , 0.01
-
-        print(f"")
-        delta = [last_delta, right - left]
-        Log.add_to_current_data(left=left)
-        Log.add_to_current_data(right=right)
-        Log.add_to_current_data(P=kp*delta[1])
-        Log.add_to_current_data(I=ki*(delta[0] + delta[1])*dt)
-        Log.add_to_current_data(D=kd*(delta[0] - delta[1])/dt)
-        print(f"{left = }, {right = }, P = {kp*delta[1]:.2f}, I = {ki*(delta[0] + delta[1])*dt:.2f}, D = {kd*(delta[0] - delta[1])/dt:.2f}")
-        s = kp*delta[1] + ki*(delta[0] + delta[1])*dt - kd*(delta[0] - delta[1])/dt
-        mybot.powerboard.set_speed(100 + s  , 100 - s)
-
-        return delta[1]
-
-        
-
     test = True
 
     while test:
@@ -43,7 +40,7 @@ if __name__ == "__main__":
         last_delta = go_straight(last_delta)
         Log.write_current_data()
 
-        if mybot.sonars.read_4_sonars()[0] < 40:
+        if mybot.sonars.read_4_sonars()[0] < 50:
             mybot.powerboard.set_speed(0, 0)
             time.sleep(1)
 
@@ -51,7 +48,7 @@ if __name__ == "__main__":
                 mybot.turn_left()
                 time.sleep(0.3)
 
-            if mybot.sonars.read_left() <= mybot.sonars.read_right() :
+            else:
                 mybot.turn_right()
                 time.sleep(0.3)
 
